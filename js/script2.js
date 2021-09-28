f0031176fa460aac3449c14bd18a3fc53321a01a
Eric Rion <erion42@gmail.com>
2021-09-28 18:09:38 -0400

    Scrapped original build. Going with Bootstrap for prettier UI. Rebuilding JS so it works better.

---
+++ js/script2.js
@@ -0,0 +1,79 @@
+// variables here
+var searchedCities = [];
+var cityInput;
+var selected;
+var todayDate = moment();
+const input = document.querySelector('input');
+
+
+// Functions established here:
+
+// button functions
+document.getElementById("submit").addEventListener("click", citySearch);
+
+function init() {
+    // Restored Saved History and replaces search results
+    if (JSON.parse(localStorage.getItem("searchHistory")) != null) {
+        restoreData();
+        replaceCity();
+    }
+}
+
+function storeData() {
+    localStorage.setItem("searchHistory", JSON.stringify(searchedCities));  
+}
+
+function restoreData() {
+    searchedCities = JSON.parse(localStorage.getItem("searchHistory"));
+}
+
+function citySearch() {
+    // Sets variable for input
+    cityInput = input.value.toUpperCase();
+    // Adds input to front of city array
+    searchedCities.unshift(cityInput);    
+    replaceCity();
+    storeData();
+}
+
+function replaceCity() {
+    if (searchedCities[0] != null) {
+        selected = searchedCities[0];
+        document.querySelector(".currentCity").innerHTML = cityInput
+        document.querySelector(".curCityTemp").innerHTML = cityInput + " Temperature";
+        document.querySelector(".curCityWind").innerHTML = cityInput + " Wind";
+        document.querySelector(".curCityHumid").innerHTML = cityInput + " Humidity";
+        document.querySelector(".curCityUV").innerHTML = cityInput + " UV";
+        document.querySelector(".cityID1").innerHTML = searchedCities[0];
+      }
+      if (searchedCities[1] != null) {
+        document.querySelector(".cityID2").innerHTML = searchedCities[1];
+      }
+      if (searchedCities[2] != null) {
+        document.querySelector(".cityID3").innerHTML = searchedCities[2];
+      }
+      if (searchedCities[3] != null) {
+        document.querySelector(".cityID4").innerHTML = searchedCities[3];
+      }
+      if (searchedCities[4] != null) {
+        document.querySelector(".cityID5").innerHTML = searchedCities[4];
+      }
+      if (searchedCities[5] != null) {
+        document.querySelector(".cityID6").innerHTML = searchedCities[5];
+      }
+      if (searchedCities[6] != null) {
+        document.querySelector(".cityID7").innerHTML = searchedCities[6];
+      }
+      if (searchedCities[7] != null) {
+        document.querySelector(".cityID8").innerHTML = searchedCities[7];
+      }
+      if (searchedCities.length > 8) {
+        searchedCities.pop();
+      }    
+}
+
+// Functions called here:
+
+// call initialization function
+document.addEventListener("load", init());
+
